082038bf64736a1a03f9f1e521a3b2fedea9b598
siddharthsameer <42216696+siddharthsameer@users.noreply.github.com>
2022-01-18 15:08:47 +0530

    Update ques4.java

--- ques4.java
+++ ques4.java
@@ -5,12 +5,10 @@ public class ques4
 
 	public static void main(String[] args) 
 	{
+		int n=10;
 		int sum=0;
-		for(int i=10;i>0;i--)
-		{
-			sum+=i;
-			
-		}
+		sum = (n*(n+1))/2;
+
 		System.out.println(sum);
 
 	}
